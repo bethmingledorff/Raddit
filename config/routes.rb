ac33540ce8921fcbf6f80098c5ef8ccb3c538d11
bethmingledorff <beth.mingledorff@gmail.com>
2015-08-25 13:40:50 -0500

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  resources :comments
   devise_for :users
   root to: 'links#index'
   resources :links do
     member do
-      put "like", to: "links#upvote"
-      put "dislike", to: "links#downvote"
+      put "like",     to: "links#upvote"
+      put "dislike",  to: "links#downvote"
     end
+    resources :comments
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
